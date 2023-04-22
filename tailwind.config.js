28be4c9164d79b99600e393d86ea5500945c5ac8
John Wilson <john.solamo3@gmail.com>
2023-04-22 18:57:47 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'bleh': '#125b9e',
+        'GRAY': '#565656',
+        'darkBleh': '#0e4679'
+      }
+    },
+  },
+  plugins: [],
+}
+
